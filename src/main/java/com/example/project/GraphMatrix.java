c09b028b6d9fde97291ac9b040b864f4139bb2ea
Gustavo <gordono@unsa.edu.pe>
2022-08-23 23:52:18 -0500

    Refactor in TAD implementation and implemented countConnectedComponents function

--- src/main/java/com/example/project/GraphMatrix.java
+++ src/main/java/com/example/project/GraphMatrix.java
@@ -1,49 +1,41 @@
 package com.example.project;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class GraphMatrix implements Graph {
-
     private int numVertices;
     private int[][] adjacency;
 
     public GraphMatrix(int numVertices) {
         this.numVertices = numVertices;
         this.adjacency = new int[numVertices][numVertices];
+    }
 
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                this.adjacency[i][j] = 0;
-            }
-        }
+    public boolean vertexDoesExist(int aVertex) {
+        return aVertex >= 0 && aVertex < this.numVertices;
     }
 
     @Override
     public boolean addEdge(int from, int to) {
-        if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+        if(this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+            // As it is not a directed graph
             this.adjacency[from][to] = 1;
             this.adjacency[to][from] = 1;
-            return true;
+            return true; // Edge was added successfully
         }
-        return false;
+        return false; // 'from' or 'to' are not in range
     }
 
     @Override
     public boolean removeEdge(int from, int to) {
-        if (this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+        if(this.vertexDoesExist(from) && this.vertexDoesExist(to)) {
+            // As it is not a directed graph
             this.adjacency[from][to] = 0;
             this.adjacency[to][from] = 0;
-            return true;
+            return true; // Edge was removed successfully
         }
-        return false;
-    }
-
-    public boolean vertexDoesExist(int aVertex) {
-        if (aVertex >= 0 && aVertex < this.numVertices) {
-            return true;
-        } else {
-            return false;
-        }
+        return false; // 'from' or 'to' are not in range
     }
 
     public ArrayList<Integer> depthFirstSearch(int n) {
@@ -52,8 +44,8 @@ public class GraphMatrix implements Graph {
 
     public ArrayList<Integer> depthFirstSearch(int n, ArrayList<Integer> visited) {
         visited.add(n);
-        for (int i = 0; i < this.numVertices; i++) {
-            if (this.adjacency[n][i] == 1 && !visited.contains(i)) {
+        for(int i = 0; i < this.numVertices; i++) {
+            if(this.adjacency[n][i] == 1 && !visited.contains(i)) {
                 depthFirstSearch(i, visited);
             }
         }
@@ -62,14 +54,14 @@ public class GraphMatrix implements Graph {
 
     public String toString() {
         String s = "    ";
-        for (int i = 0; i < this.numVertices; i++) {
+        for(int i = 0; i < this.numVertices; i++) {
             s = s + String.valueOf(i) + " ";
         }
         s = s + " \n";
 
-        for (int i = 0; i < this.numVertices; i++) {
+        for(int i = 0; i < this.numVertices; i++) {
             s = s + String.valueOf(i) + " : ";
-            for (int j = 0; j < this.numVertices; j++) {
+            for(int j = 0; j < this.numVertices; j++) {
                 s = s + String.valueOf(this.adjacency[i][j]) + " ";
             }
             s = s + "\n";
@@ -78,12 +70,27 @@ public class GraphMatrix implements Graph {
     }
 
     public int countConnectedComponents() {
+        HashSet<Integer> already = new HashSet<Integer>(); // Already Visited Nodes from all DFS results
+        int nV = 0; // Not Visited index
+        int cc = 0; // Connected Components
+        while(already.size() != this.numVertices) {
+            ArrayList<Integer> DFS = depthFirstSearch(nV);
+            for(int num: DFS) {
+                already.add(num);
+            }
 
-        return -1;
+            for(int i = 0; i < this.numVertices; i++) {
+                if(!DFS.contains(i) && !already.contains(i)) {
+                    nV = i;
+                }
+            }
+            cc++;
+        }
+        return cc;
     }
 
     public static void main(String args[]) {
-        GraphMatrix graph = new GraphMatrix(5);
+        GraphMatrix graph = new GraphMatrix(6);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(0, 3);
@@ -96,5 +103,4 @@ public class GraphMatrix implements Graph {
         System.out.println("DFS:");
         System.out.println(graph.depthFirstSearch(0));
     }
-
 }
